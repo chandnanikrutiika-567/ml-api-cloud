4a93c6ed4cd4869facc3ead48528066f32d2f9e2
krutiika chandnani <chandnanikrutiika@gmail.com>
2026-04-21 00:55:11 +0530

    fix flask app for EB deployment (app variable, POST route, gunicorn)

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 import joblib
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-# Load the trained model into memory
 model = joblib.load('sentiment_model.joblib')
 
-@application.route('/predict')
+@app.route('/')
+def home():
+    return "API is running"
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    # Parse the incoming JSON request
     data = request.get_json()
     text = data.get('text', '')
 
@@ -17,16 +19,12 @@ def predict():
             'error': 'No text provided. Please send a JSON with a "text" key.'
         }), 400
 
-    # Make a prediction
     prediction = model.predict([text])[0]
 
-    # Return the result
     return jsonify({
         'input_text': text,
         'sentiment_prediction': prediction
     })
 
-
 if __name__ == '__main__':
-    # Listen on all network interfaces (important for Docker)
     app.run(host='0.0.0.0', port=5000)
